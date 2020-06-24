30055911dbcfd35227011ba9a80b831da778ebeb
akram-gorashi <akramgorashi6@gmail.com>
2020-06-25 01:33:09 +0200

    [update]

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -4,23 +4,23 @@ using Microsoft.AspNetCore.Http;
 
 namespace Al_Delal.Api.Models
 {
-    public class Vehicle
-    {
-        public int Id { get; set; }
-        public DateTime DateAdded { get; set; }
-        public string Make { get; set; }
-        public string TypeOfCar { get; set; }
-        public int Mileage { get; set; }
-        public string Color { get; set; }
-        public string Condition { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public string Fuel { get; set; }
-        public string Transmission { get; set; }
-        public string Price { get; set; }
-        public string Description { get; set; }
-      
-        public User User { get; set; }
-        public int UserId { get; set; }
-    }
+   public class Vehicle
+   {
+      public int Id { get; set; }
+      public DateTime DateAdded { get; set; }
+      public string Make { get; set; }
+      public string TypeOfCar { get; set; }
+      public int Mileage { get; set; }
+      public string Color { get; set; }
+      public string Condition { get; set; }
+      public string Model { get; set; }
+      public int Year { get; set; }
+      public string Fuel { get; set; }
+      public string Transmission { get; set; }
+      public string Price { get; set; }
+      public string Description { get; set; }
+      public string PhoneNumber { get; set; }
+      public User User { get; set; }
+      public int UserId { get; set; }
+   }
 }
